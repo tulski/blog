b582d6cdb0ee0a785385f90cd2a2a94f331d4a91
tulski <me@tulski.com>
2025-03-01 20:59:14 +0100

    Spring: Binding maps from environment variables and YAML files

--- app/highlight.ts
+++ app/highlight.ts
@@ -4,9 +4,13 @@ import javascript from "highlight.js/lib/languages/javascript";
 import typescript from "highlight.js/lib/languages/typescript";
 import java from "highlight.js/lib/languages/java";
 import plaintext from "highlight.js/lib/languages/plaintext";
+import yaml from "highlight.js/lib/languages/yaml";
+import diff from "highlight.js/lib/languages/diff";
 
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("typescript", typescript);
 hljs.registerLanguage("java", java);
 hljs.registerLanguage("text", plaintext);
 hljs.registerLanguage("plaintext", plaintext);
+hljs.registerLanguage("yaml", yaml);
+hljs.registerLanguage("diff", diff);
